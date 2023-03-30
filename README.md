c4b163f4930e174fe0f22db944124b8a9b12fb18
ncumovi <451299572@qq.com>
2023-03-30 13:59:21 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# commonVuePro
-一个基于vue@2.x + Vue-cli@4.x + axios + vue-router + vuex + elementUI + scss 的工程化项目，开箱即用！ 
+# common-vue-pro
+一个基于vue@2.x + Vue-cli@4.x + axios + vue-router + vuex + elementUI + scss 的工程化项目，开箱即用！
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Node
+V14.15.0
---
+++ vue.config.js
@@ -0,0 +1,112 @@
+"use strict";
+const path = require("path");
+const webpack = require("webpack");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  publicPath: "/",
+  outputDir: "dist",
+  assetsDir: "static",
+  // lintOnSave: process.env.NODE_ENV === 'development',
+  lintOnSave: false,
+  productionSourceMap: true,
+  devServer: {
+    port: 8888,
+    open: true,
+    disableHostCheck: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    proxy: {
+      "/api": {
+        target: "http://1.1.1.1",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "/api",
+        },
+      },
+    },
+  },
+  configureWebpack: {
+    name: "vue工程项目",
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+      },
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        // 每一对键值都会被注入到代码当中
+        BUILD_ENV: JSON.stringify(process.env.NODE_ENV) || "'dev'", //传入字符串
+      }),
+    ],
+  },
+  chainWebpack(config) {
+    config.plugin("preload").tap(() => [
+      {
+        rel: "preload",
+        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+        include: "initial",
+      },
+    ]);
+
+    // when there are many pages, it will cause too many meaningless requests
+    config.plugins.delete("prefetch");
+
+    // set svg-sprite-loader
+    config.module.rule("svg").exclude.add(resolve("src/icons")).end();
+    config.module
+      .rule("icons")
+      .test(/\.svg$/)
+      .include.add(resolve("src/icons"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]",
+      })
+      .end();
+
+    config.when(process.env.NODE_ENV !== "development", (config) => {
+      config
+        .plugin("ScriptExtHtmlWebpackPlugin")
+        .after("html")
+        .use("script-ext-html-webpack-plugin", [
+          {
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+            inline: /runtime\..*\.js$/,
+          },
+        ])
+        .end();
+      config.optimization.splitChunks({
+        chunks: "all",
+        cacheGroups: {
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: "initial", // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: "chunk-elementUI", // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/, // in order to adapt to cnpm
+          },
+          commons: {
+            name: "chunk-commons",
+            test: resolve("src/components"), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      });
+      // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+      config.optimization.runtimeChunk("single");
+    });
+  },
+};
